adce5a1f6a32021700da7fbd44cebe324a4d031a
itan <itanfelz@uc.cl>
2023-05-23 20:45:59 -0400

    push notification ¿

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-const express = require('express');
-const path = require('path');
-
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000; // El número de puerto en el que se ejecutará el servidor
 
 // Configurar el middleware para servir archivos estáticos
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Ruta de inicio
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 // Iniciar el servidor
 app.listen(port, () => {
-  console.log(`Servidor en ejecución en http://localhost:${port}`);
+    console.log(`Servidor en ejecución en http://localhost:${port}`);
 });
